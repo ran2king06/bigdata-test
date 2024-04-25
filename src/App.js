dbd00eed650bebc3782d41eacf6304687169b5ec
ran2king06 <ran2king06@gmail.com>
2024-04-26 00:12:10 +0800

    feat: import MUI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Button from '@mui/material/Button';
+
+import ColumnGroupingTable from './components/table';
+import logo from './logo.svg';
 
 function App() {
   return (
@@ -9,6 +13,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button variant="contained">Hello world</Button>
+        <ColumnGroupingTable></ColumnGroupingTable>
         <a
           className="App-link"
           href="https://reactjs.org"
