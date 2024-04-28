99727ad6ba4c19d0fe19ede81881a1dceaa64f99
ran2king06 <ran2king06@gmail.com>
2024-04-29 00:15:30 +0800

    test

--- src/js/api.js
+++ src/js/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
